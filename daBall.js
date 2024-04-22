2ba9ee5aa8736cbb4ffbe4408b2e6c5cc823e9ba
Skorp <105490480+Skorplul@users.noreply.github.com>
2024-04-22 11:23:19 +0200

    Update daBall.js
    Converting the poem john maynard from the main script file to here

--- daBall.js
+++ daBall.js
@@ -6,3 +6,44 @@ export const ballAnswers = [
     "Why not",
     "Fuck off",
 ];
+
+export const johnGedErst = `John Maynard!
+„Wer ist John Maynard?“
+„John Maynard war unser Steuermann,
+aushielt er, bis er das Ufer gewann,
+er hat uns gerettet, er trägt die Kron',
+er starb für uns, unsre Liebe sein Lohn.
+John Maynard.“
+
+Die „Schwalbe“ fliegt über den Erie-See,
+Gischt schäumt um den Bug wie Flocken von Schnee;
+von Detroit fliegt sie nach Buffalo -
+die Herzen aber sind frei und froh,
+und die Passagiere mit Kindern und Fraun
+im Dämmerlicht schon das Ufer schaun,
+und plaudernd an John Maynard heran
+tritt alles: „Wie weit noch, Steuermann?“
+Der schaut nach vorn und schaut in die Rund:
+„Noch dreißig Minuten ... Halbe Stund.“
+
+Alle Herzen sind froh, alle Herzen sind frei -
+da klingt's aus dem Schiffsraum her wie Schrei,
+„Feuer!“ war es, was da klang,
+ein Qualm aus Kajüt und Luke drang,
+ein Qualm, dann Flammen lichterloh,
+und noch zwanzig Minuten bis Buffalo.
+
+Und die Passagiere, bunt gemengt,
+am Bugspriet stehn sie zusammengedrängt,
+am Bugspriet vorn ist noch Luft und Licht,
+am Steuer aber lagert sich´s dicht,
+und ein Jammern wird laut: „Wo sind wir? wo?“
+Und noch fünfzehn Minuten bis Buffalo. -
+
+Der Zugwind wächst, doch die Qualmwolke steht,
+der Kapitän nach dem Steuer späht,
+er sieht nicht mehr seinen Steuermann,
+aber durchs Sprachrohr fragt er an:
+„Noch da, John Maynard?“
+„Ja, Herr. Ich bin.“
+`
